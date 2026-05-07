071d11714e0442506de6ff6a05d1581b2e988c2a
Spiral <po99h6@gmail.com>
2026-05-07 06:09:16 +0000

    Remove firmware files from vendor partition

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -2,34 +2,3 @@
 # Automatically generated file. DO NOT MODIFY
 #
 
-AB_OTA_PARTITIONS += \
-    abl \
-    aop \
-    aop_config \
-    bluetooth \
-    cpucp \
-    cpucp_dtb \
-    devcfg \
-    dsp \
-    engineering_cdt \
-    featenabler \
-    hyp \
-    imagefv \
-    keymaster \
-    modem \
-    oplus_sec \
-    pdp \
-    pdp_cdb \
-    pvmfw \
-    qupfw \
-    shrm \
-    soccp_dcd \
-    soccp_debug \
-    splash \
-    spuservice \
-    tz \
-    uefi \
-    uefisecapp \
-    xbl \
-    xbl_config \
-    xbl_ramdump
